f7c5365fb5812ba6bf229aaede0f5a02acc1ace2
chr <2720844361@qq.com>
2021-03-31 15:09:34 +0800

    e

--- src/w1/Task4.java
+++ src/w1/Task4.java
@@ -45,51 +45,51 @@ public class Task4 {
 		//利用动态规划算法解决0-1背包问题
 		int value[]= {408,921,1329,11,998,1009,104,839,943,299,374,673,703,954,1657,425,950,1375,430,541,971,332,483,815,654,706,1360,956,992,1948};
 		int weight []= {508,1021,1321,111,1098,1196,204,939,1107,399,474,719,803,1054,1781,525,1050,1362,530,641,903,432,583,894,754,806,1241,1056,1092,1545};
-		int w = 10149;//表示背包的最大承重
+		int w = 10149;//背包容量
 		int n = weight.length;//表示物品的个数
-		int[][] maxVal = new int[n + 1][w + 1];//v[i][j]表示在前i个物品中能够装入容量为j的背包中的最大价值
-		int[][] mark = new int[n + 1][w + 1];//记录哪个物品被放进背包
+		int mvaluel[][] = new int[n + 1][w + 1];//最大价值
+		int wup[][] = new int[n + 1][w + 1];//放入背包的物品
 		int c=0;//记录背包里的物品
-		//初始化条件
+		//初始化
 		for (int i = 0; i < w + 1; i++) {
-			maxVal[0][i] = 0;
+			mvaluel[0][i] = 0;
 		}
 		for (int i = 0; i < n + 1; i++) {
-			maxVal[i][0] = 0;
+			mvaluel[i][0] = 0;
 		}
 
 		//动态规划
 
-		for (int i = 1; i < maxVal.length; i++) {
-			for (int j = 1; j < maxVal[i].length; j++) {
+		for (int i=1; i <mvaluel.length;i++) {
+			for (int j=1; j < mvaluel[i].length; j++) {
+				//如果物品的重量小于当前背包的容量
 				if (weight[i - 1] > j) {
-					//如果第i-1个物品(因为i从1开始)的重量小于当前背包重量
-					maxVal[i][j] = maxVal[i - 1][j];
-				} else {
-					if (maxVal[i - 1][j] < value[i - 1] + maxVal[i - 1][j - weight[i - 1]]) {
-						//如果上一次规划的最大价值小于当前物品的价值和上一次规划的重量减去当前物品重量的价值总和。
-						maxVal[i][j] = value[i - 1] + maxVal[i - 1][j - weight[i - 1]];
-						mark[i][j] = 1;//记录
+					mvaluel[i][j] = mvaluel[i - 1][j];
+				} else {//如果物品的重量大于当前背包的容量
+					//如果上一次最大价值<当前物品的价值+上一次重量-当前物品重量的价值总和。
+					if (mvaluel[i - 1][j] < value[i - 1] + mvaluel[i - 1][j - weight[i - 1]]) {
+						mvaluel[i][j] = value[i - 1] + mvaluel[i - 1][j - weight[i - 1]];
+						wup[i][j] = 1;//将物品放入背包
 					} else {
-						maxVal[i][j] = maxVal[i - 1][j];
+						mvaluel[i][j] = mvaluel[i - 1][j];
 					}
 				}
 			}
 		}
 		//查看二维数组
-		for (int[] arr : maxVal) {
+		for (int arr[] : mvaluel) {
 			System.out.println(Arrays.toString(arr));
 		}
-		//列出最终方案
-		int i = mark.length - 1;
-		int j = mark[0].length - 1;
-		for(c=0;c<3;c--) {
-			while (i > 0 && j > 0) {
-				if (mark[i][j] == 1) {
-					System.out.println("第" + i + "个商品放入了背包");
-					j -= weight[i - 1];
+		//求出最优解
+		int a = wup.length - 1;
+		int b = wup[0].length - 1;
+		for(c=0;c<3;c--) {//只在背包中放入三个物品
+			while (a > 0 && b > 0) {
+				if (wup[a][b] == 1) {
+					System.out.println("将第" + a + "个商品放入了背包");
+					 b=b- weight[a - 1];
 				}
-				i--;
+				a--;
 			}
 		}
 		long timeEnd = System.currentTimeMillis();
