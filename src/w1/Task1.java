626d26f4e4a8775155dd58f93c5addde268f20bd
cxl <2547406431@qq.com>
2021-04-14 08:18:43 +0800

    提交信息

--- src/w1/Task1.java
+++ src/w1/Task1.java
@@ -4,36 +4,36 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.LineNumberReader;
-//任务1：可正确读入实验数据文件的有效D{0-1}KP数据；
+//浠诲姟1锛氬彲姝ｇ‘璇诲叆瀹為獙鏁版嵁鏂囦欢鐨勬湁鏁圖{0-1}KP鏁版嵁锛�
 public class Task1 { 
 	 public static void main(String[] args) throws IOException {
-         long start = System.currentTimeMillis();//计算导入数据的时间
-         File file = new File( "C:\\Users\\Administrator\\eclipse-workspace\\work02\\src\\txts\\idkp1-10.txt" ); //文件路径
-         System.out.println("读取IDKP0:");
-         FileReader fR = new FileReader(file);//文件读取
-         LineNumberReader reader = new LineNumberReader(fR);//统计行号
+         long start = System.currentTimeMillis();//璁＄畻瀵煎叆鏁版嵁鐨勬椂闂�
+         File file = new File( "C:\\Users\\Administrator\\eclipse-workspace\\work02\\src\\txts\\idkp1-10.txt" ); //鏂囦欢璺緞
+         System.out.println("璇诲彇IDKP0:");
+         FileReader fR = new FileReader(file);//鏂囦欢璇诲彇
+         LineNumberReader reader = new LineNumberReader(fR);//缁熻琛屽彿
          int now1 = 0 ;  
-         int number1 = 6 ; //设置指定行数（价值）
-         String txt= "" ;      //用来存储得到的不同的价值   
+         int number1 = 6 ; //璁剧疆鎸囧畾琛屾暟锛堜环鍊硷級
+         String txt= "" ;      //鐢ㄦ潵瀛樺偍寰楀埌鐨勪笉鍚岀殑浠峰��   
          while (txt != null ) {
         	 now1++;       
-             txt = reader.readLine();//按行读取：读取每一行
-             if (now1 == number1) {//当读取的行数=指定的行数，则输出改行的内容
-                 System.out.println( "The profit of itmes are（读取的价值如下）:" +"\n"+  txt + "\n" );
+             txt = reader.readLine();//鎸夎璇诲彇锛氳鍙栨瘡涓�琛�
+             if (now1 == number1) {//褰撹鍙栫殑琛屾暟=鎸囧畾鐨勮鏁帮紝鍒欒緭鍑烘敼琛岀殑鍐呭
+                 System.out.println( "The profit of itmes are锛堣鍙栫殑浠峰�煎涓嬶級:" +"\n"+  txt + "\n" );
              }
          }
          reader.close();
-         fR.close();//关闭文件
+         fR.close();//鍏抽棴鏂囦欢
          FileReader fR2 = new FileReader(file);
          LineNumberReader reader2 = new LineNumberReader(fR2);
-         int number2 = 8 ; //设置指定行数（重量）
-         String txt2= "" ;     //用来存储得到的不同的重量
+         int number2 = 8 ; //璁剧疆鎸囧畾琛屾暟锛堥噸閲忥級
+         String txt2= "" ;     //鐢ㄦ潵瀛樺偍寰楀埌鐨勪笉鍚岀殑閲嶉噺
          int now2= 0 ;
          while (txt2 != null ) {
              now2++;
-             txt2 = reader2.readLine();//按行读取：读取每一行
-             if (now2 == number2) {//当读取的行数=指定的行数，则输出改行的内容
-                 System.out.println( "The weight of itmes are（读取的重量值如下）:" + "\n" + txt2 + "\n" );
+             txt2 = reader2.readLine();//鎸夎璇诲彇锛氳鍙栨瘡涓�琛�
+             if (now2 == number2) {//褰撹鍙栫殑琛屾暟=鎸囧畾鐨勮鏁帮紝鍒欒緭鍑烘敼琛岀殑鍐呭
+                 System.out.println( "The weight of itmes are锛堣鍙栫殑閲嶉噺鍊煎涓嬶級:" + "\n" + txt2 + "\n" );
              }
          }
          reader2.close();
@@ -42,31 +42,31 @@ public class Task1 {
          
          
          
-         System.out.println("读取IDKP1:");
-         FileReader fR11 = new FileReader(file);//文件读取
-         LineNumberReader reader11 = new LineNumberReader(fR11);//统计行号
+         System.out.println("璇诲彇IDKP1:");
+         FileReader fR11 = new FileReader(file);//鏂囦欢璇诲彇
+         LineNumberReader reader11 = new LineNumberReader(fR11);//缁熻琛屽彿
          int now11 = 0 ;  
-         int number11 = 14 ; //设置指定行数（价值）
-         String txt11= "" ;      //用来存储得到的不同的价      
+         int number11 = 14 ; //璁剧疆鎸囧畾琛屾暟锛堜环鍊硷級
+         String txt11= "" ;      //鐢ㄦ潵瀛樺偍寰楀埌鐨勪笉鍚岀殑浠�      
          while (txt11 != null ) {
         	 now11++;
              txt11 = reader11.readLine();
              if (now11 == number11) {
-                 System.out.println( "The profit of itmes are（读取的价值如下）:" +"\n"+  txt11 + "\n" );
+                 System.out.println( "The profit of itmes are锛堣鍙栫殑浠峰�煎涓嬶級:" +"\n"+  txt11 + "\n" );
              }
          }
          reader11.close();
          fR11.close();
          FileReader fR12 = new FileReader(file);
          LineNumberReader reader12 = new LineNumberReader(fR12);
-         int number12 = 16 ; //设置指定行数（重量）
-         String txt12= "" ;     //用来存储得到的不同的重量
+         int number12 = 16 ; //璁剧疆鎸囧畾琛屾暟锛堥噸閲忥級
+         String txt12= "" ;     //鐢ㄦ潵瀛樺偍寰楀埌鐨勪笉鍚岀殑閲嶉噺
          int now12= 0 ;
          while (txt12 != null ) {
              now12++;
              txt12 = reader12.readLine();
              if (now12 == number12) {
-                 System.out.println( "The weight of itmes are（读取的重量值如下）:" + "\n" + txt12 + "\n" );
+                 System.out.println( "The weight of itmes are锛堣鍙栫殑閲嶉噺鍊煎涓嬶級:" + "\n" + txt12 + "\n" );
              }
          }
          reader12.close();
@@ -76,31 +76,31 @@ public class Task1 {
          
          
          
-         System.out.println("读取IDKP2:");
-         FileReader fR21 = new FileReader(file);//文件读取
-         LineNumberReader reader21 = new LineNumberReader(fR21);//统计行号
+         System.out.println("璇诲彇IDKP2:");
+         FileReader fR21 = new FileReader(file);//鏂囦欢璇诲彇
+         LineNumberReader reader21 = new LineNumberReader(fR21);//缁熻琛屽彿
          int now21 = 0 ;  
-         int number21 = 22 ; //设置指定行数（价值）
-         String txt21= "" ;      //用来存储得到的不同的价      
+         int number21 = 22 ; //璁剧疆鎸囧畾琛屾暟锛堜环鍊硷級
+         String txt21= "" ;      //鐢ㄦ潵瀛樺偍寰楀埌鐨勪笉鍚岀殑浠�      
          while (txt21 != null ) {
         	 now21++;
              txt21 = reader21.readLine();
              if (now21 == number21) {
-                 System.out.println( "The profit of itmes are（读取的价值如下）:" +"\n"+  txt21 + "\n" );
+                 System.out.println( "The profit of itmes are锛堣鍙栫殑浠峰�煎涓嬶級:" +"\n"+  txt21 + "\n" );
              }
          }
          reader21.close();
          fR21.close();
          FileReader fR22 = new FileReader(file);
          LineNumberReader reader22 = new LineNumberReader(fR22);
-         int number22 = 24 ; //设置指定行数（重量）
-         String txt22= "" ;     //用来存储得到的不同的重量
+         int number22 = 24 ; //璁剧疆鎸囧畾琛屾暟锛堥噸閲忥級
+         String txt22= "" ;     //鐢ㄦ潵瀛樺偍寰楀埌鐨勪笉鍚岀殑閲嶉噺
          int now22= 0 ;
          while (txt12 != null ) {
              now22++;
              txt12 = reader12.readLine();
              if (now22 == number22) {
-                 System.out.println( "The weight of itmes are（读取的重量值如下）:" + "\n" + txt22 + "\n" );
+                 System.out.println( "The weight of itmes are锛堣鍙栫殑閲嶉噺鍊煎涓嬶級:" + "\n" + txt22 + "\n" );
              }
          }
          reader22.close();
@@ -108,38 +108,38 @@ public class Task1 {
          
          
          
-         System.out.println("读取IDKP3:");
-         FileReader fR31 = new FileReader(file);//文件读取
-         LineNumberReader reader31 = new LineNumberReader(fR31);//统计行号
+         System.out.println("璇诲彇IDKP3:");
+         FileReader fR31 = new FileReader(file);//鏂囦欢璇诲彇
+         LineNumberReader reader31 = new LineNumberReader(fR31);//缁熻琛屽彿
          int now31 = 0 ;  
-         int number31 = 30 ; //设置指定行数（价值）
-         String txt31= "" ;      //用来存储得到的不同的价      
+         int number31 = 30 ; //璁剧疆鎸囧畾琛屾暟锛堜环鍊硷級
+         String txt31= "" ;      //鐢ㄦ潵瀛樺偍寰楀埌鐨勪笉鍚岀殑浠�      
          while (txt31 != null ) {
         	 now31++;
              txt31 = reader31.readLine();
              if (now31 == number31) {
-                 System.out.println( "The profit of itmes are（读取的价值如下）:" +"\n"+  txt31 + "\n" );
+                 System.out.println( "The profit of itmes are锛堣鍙栫殑浠峰�煎涓嬶級:" +"\n"+  txt31 + "\n" );
              }
          }
          reader31.close();
          fR31.close();
          FileReader fR32 = new FileReader(file);
          LineNumberReader reader32 = new LineNumberReader(fR32);
-         int number32 = 32 ; //设置指定行数（重量）
-         String txt32= "" ;     //用来存储得到的不同的重量
+         int number32 = 32 ; //璁剧疆鎸囧畾琛屾暟锛堥噸閲忥級
+         String txt32= "" ;     //鐢ㄦ潵瀛樺偍寰楀埌鐨勪笉鍚岀殑閲嶉噺
          int now32= 0 ;
          while (txt32 != null ) {
              now32++;
              txt32 = reader32.readLine();
              if (now32 == number32) {
-                 System.out.println( "The weight of itmes are（读取的重量值如下）:" + "\n" + txt32 + "\n" );
+                 System.out.println( "The weight of itmes are锛堣鍙栫殑閲嶉噺鍊煎涓嬶級:" + "\n" + txt32 + "\n" );
              }
          }
          reader32.close();
          fR32.close();
          
          long end = System.currentTimeMillis();
-         System.out.println( "总共花费：" + (end - start)+ "ms" );
+         System.out.println( "本程序所使用的时间为：" + (end - start)+ "ms" );
          
      }
 }
--- src/w1/Task4.java
+++ src/w1/Task4.java
@@ -9,15 +9,15 @@ import java.io.LineNumberReader;
 import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.Scanner;
-//任务4：用户能够自主选择动态规划算法、回溯算法求解指定D{0-1} KP数据的最优解和求解时间（以秒为单位）；
+//浠诲姟4锛氱敤鎴疯兘澶熻嚜涓婚�夋嫨鍔ㄦ�佽鍒掔畻娉曘�佸洖婧畻娉曟眰瑙ｆ寚瀹欴{0-1} KP鏁版嵁鐨勬渶浼樿В鍜屾眰瑙ｆ椂闂达紙浠ョ涓哄崟浣嶏級锛�
 public class Task4 { 
 	public static void main(String[] args) throws IOException {		
 		long timeStart = System.currentTimeMillis();
-		File file = new File( "C:\\Users\\Administrator\\eclipse-workspace\\work02\\src\\txts\\idkp1-10.txt" ); //文件路径
+		File file = new File( "C:\\Users\\Administrator\\eclipse-workspace\\work02\\src\\txts\\idkp1-10.txt" ); //鏂囦欢璺緞
 		FileReader fileReader = new FileReader(file);
 		LineNumberReader reader = new LineNumberReader(fileReader);
-		int number1 = 6 ;        //设置指定需要读取的行数（价值）
-		String txt = "";              //用来存储得到的不同的价值
+		int number1 = 6 ;        //璁剧疆鎸囧畾闇�瑕佽鍙栫殑琛屾暟锛堜环鍊硷級
+		String txt = "";              //鐢ㄦ潵瀛樺偍寰楀埌鐨勪笉鍚岀殑浠峰��
 		int lines = 0 ;
 		while (txt != null ) {
 			lines++;
@@ -26,11 +26,11 @@ public class Task4 {
 				System.out.println( "The profit of itmes are:" +  txt + "\n" );
 			}
 		}
-		File file2 = new File( "C:\\Users\\Administrator\\eclipse-workspace\\work02\\src\\txts\\idkp1-10.txt" ); //文件路径
+		File file2 = new File( "C:\\Users\\Administrator\\eclipse-workspace\\work02\\src\\txts\\idkp1-10.txt" ); //鏂囦欢璺緞
 		FileReader fileReader2 = new FileReader(file2);
 		LineNumberReader reader2 = new LineNumberReader(fileReader2);
-		int number2 = 8 ; //设置指定行数（重量）
-		String txt2= "" ;     //用来存储得到的不同的重量
+		int number2 = 8 ; //璁剧疆鎸囧畾琛屾暟锛堥噸閲忥級
+		String txt2= "" ;     //鐢ㄦ潵瀛樺偍寰楀埌鐨勪笉鍚岀殑閲嶉噺
 		int lines2= 0 ;
 		while (txt2 != null ) {
 			lines2++;
@@ -42,15 +42,15 @@ public class Task4 {
 		reader.close();
 		fileReader.close();
 
-		//利用动态规划算法解决0-1背包问题
+		//鍒╃敤鍔ㄦ�佽鍒掔畻娉曡В鍐�0-1鑳屽寘闂
 		int value[]= {408,921,1329,11,998,1009,104,839,943,299,374,673,703,954,1657,425,950,1375,430,541,971,332,483,815,654,706,1360,956,992,1948};
 		int weight []= {508,1021,1321,111,1098,1196,204,939,1107,399,474,719,803,1054,1781,525,1050,1362,530,641,903,432,583,894,754,806,1241,1056,1092,1545};
-		int w = 10149;//背包容量
-		int n = weight.length;//表示物品的个数
-		int mvaluel[][] = new int[n + 1][w + 1];//最大价值
-		int wup[][] = new int[n + 1][w + 1];//放入背包的物品
-		int c=0;//记录背包里的物品
-		//初始化
+		int w = 10149;//鑳屽寘瀹归噺
+		int n = weight.length;//琛ㄧず鐗╁搧鐨勪釜鏁�
+		int mvaluel[][] = new int[n + 1][w + 1];//鏈�澶т环鍊�
+		int wup[][] = new int[n + 1][w + 1];//鏀惧叆鑳屽寘鐨勭墿鍝�
+		int c=0;//璁板綍鑳屽寘閲岀殑鐗╁搧
+		//鍒濆鍖�
 		for (int i = 0; i < w + 1; i++) {
 			mvaluel[0][i] = 0;
 		}
@@ -58,41 +58,42 @@ public class Task4 {
 			mvaluel[i][0] = 0;
 		}
 
-		//动态规划
+		//鍔ㄦ�佽鍒�
 
+		//排序的算法可以采用快速排序和冒泡排序
 		for (int i=1; i <mvaluel.length;i++) {
 			for (int j=1; j < mvaluel[i].length; j++) {
-				//如果物品的重量小于当前背包的容量
+				//濡傛灉鐗╁搧鐨勯噸閲忓皬浜庡綋鍓嶈儗鍖呯殑瀹归噺
 				if (weight[i - 1] > j) {
 					mvaluel[i][j] = mvaluel[i - 1][j];
-				} else {//如果物品的重量大于当前背包的容量
-					//如果上一次最大价值<当前物品的价值+上一次重量-当前物品重量的价值总和。
+				} else {//濡傛灉鐗╁搧鐨勯噸閲忓ぇ浜庡綋鍓嶈儗鍖呯殑瀹归噺
+					//濡傛灉涓婁竴娆℃渶澶т环鍊�<褰撳墠鐗╁搧鐨勪环鍊�+涓婁竴娆￠噸閲�-褰撳墠鐗╁搧閲嶉噺鐨勪环鍊兼�诲拰銆�
 					if (mvaluel[i - 1][j] < value[i - 1] + mvaluel[i - 1][j - weight[i - 1]]) {
 						mvaluel[i][j] = value[i - 1] + mvaluel[i - 1][j - weight[i - 1]];
-						wup[i][j] = 1;//将物品放入背包
+						wup[i][j] = 1;//灏嗙墿鍝佹斁鍏ヨ儗鍖�
 					} else {
 						mvaluel[i][j] = mvaluel[i - 1][j];
 					}
 				}
 			}
 		}
-		//过程
+		//杩囩▼
 		for (int arr[] : mvaluel) {
 			System.out.println(Arrays.toString(arr));
 		}
-		//求出最优解
+		//姹傚嚭鏈�浼樿В
 		int a = wup.length - 1;
 		int b = wup[0].length - 1;
-		for(c=0;c<3;c--) {//只在背包中放入三个物品
+		for(c=0;c<3;c--) {//鍙湪鑳屽寘涓斁鍏ヤ笁涓墿鍝�
 			while (a > 0 && b > 0) {
 				if (wup[a][b] == 1) {
-					System.out.println("将第" + a + "个商品放入了背包");
+					System.out.println("灏嗙" + a + "涓晢鍝佹斁鍏ヤ簡鑳屽寘");
 					 b=b- weight[a - 1];
 				}
 				a--;
 			}
 		}
 		long timeEnd = System.currentTimeMillis();
-		System.out.println( "总共花费：" + (timeEnd - timeStart)/1000+ "s" );
+		System.out.println( "鎬诲叡鑺辫垂锛�" + (timeEnd - timeStart)/1000+ "s" );
 	}
 }
